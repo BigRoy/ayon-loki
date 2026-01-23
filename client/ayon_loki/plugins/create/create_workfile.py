7431555fe561569112d8507d7e8a51010cc90c0d
Roy Nieterau <roy_nieterau@hotmail.com>
2026-01-23 23:24:41 +0100

    Cosmetics

--- client/ayon_loki/plugins/create/create_workfile.py
+++ client/ayon_loki/plugins/create/create_workfile.py
@@ -15,7 +15,7 @@ class CreateWorkfile(AutoCreator):
     label = "Workfile"
     product_type = "workfile"
     icon = "fa5.file"
-    
+
     default_variant = "Main"
 
     data_key = "AYON_workfile"
